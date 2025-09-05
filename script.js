9a5b46961bbb1bbcd716ee03f058f8913791a7b3
IknuDeer <ethanhannum10@gmail.com>
2025-09-04 20:58:51 -0700

    Add getHumanChoice function
    This function will ask the user "Rock, Paper, Scissors?",
    store the input, then return the users choice

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
 // Function getComputerChoice will randomly return "rock", "paper", or "scissors"
 function getComputerChoice() {    
-// Initialze randNum variable assigning Math.random to the randNum
+// Initialize randNum variable assigning Math.random to the randNum
     let randNum = Math.random();
 // Create if statement with the condition if randNum is less than or equal to 0.33 then return "rock"
     if (randNum <= 0.33) {
-        return "rock"
+        return "rock";
 // Else if randNum is greater than 0.33 AND if randNum is less or equal to 0.66 then return "paper"
     } else if ((randNum > 0.33) && (randNum <= 0.66)) {
-        return "paper"
+        return "paper";
 // Else return "scissors"
     } else {
-        return "scissors"
+        return "scissors";
+    }
+}
+
+// Function getHumanChoice will ask user "(R)ock, (P)aper or (S)cissors?" and return one of the choices
+function getHumanChoice() {
+// Initialize humanCoice variable assigning a prompt asking the user their choice
+    let humanChoice = prompt("(R)ock, (P)aper, (S)cissors?\nPlease Enter 'R', 'P' or 'S': ");
+// Switch statement for choice where
+    switch (humanChoice) {
+// Case "R" and Case "r" return "rock", break
+        case "r":
+        case "R":
+            return "rock";
+            break;
+// Case "P" and Case "p" return "paper", break
+        case "p":
+        case "P":
+            return "paper";
+            break;
+// Case "S" and Case "s" return "scissors", break
+        case "s":
+        case "S":
+            return "scissors";
+            break;
     }
 }
